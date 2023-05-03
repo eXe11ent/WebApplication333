7e9a73dbb450bf4a24c12640eea9d805129a7109
exe11ent <exe11ent@mail.ru>
2023-05-03 23:32:17 +0300

    Add Db

---
+++ WebApplication333/MyDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=mydb;");
+}
